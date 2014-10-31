b2f74c2cb12a284115d2c901c787b818749d4eba
bean <yu.mao@4finance.com>
2014-10-31 16:54:27 -0400

    update homepage

--- public/javascript/scriptsh.js
+++ public/javascript/scriptsh.js
@@ -22,48 +22,45 @@ $('#nav .navbar-nav li>a').click(function(){
   $('body,html').animate({scrollTop:posi},700);
 })
 
-	
-			$('#example1').accordionSlider({
-				width: 1170,
-				// orientation: "horizontal",
-				height: 525,
-				startPanel: 1,
-				shadow: 0,
-				responsiveMode: 'auto',
-				openedPanelSize: '60%',
-				maxOpenedPanelSize: '100%',
-				visiblePanels: 3,
-				closePanelsOnMouseOut: false,
-				autoplay: false
-			});
-
-			// change the responsive mode
-			$('.controls a').click(function(event) {
-				event.preventDefault();
-
-				if ($(this).hasClass('auto')) {
-					// change the responsive mode to 'auto' and remove the 'custom-responsive' class
-					$('#example1').removeClass('custom-responsive');
-					$('#example1').accordionSlider('responsiveMode', 'auto');
-
-					// change the arrows' visibility
-					$('.auto-arrow').show();
-					$('.custom-arrow').hide();
-				} else if ($(this).hasClass('custom')) {
-					// change the responsive mode to 'custom' and add the 'custom-responsive' 
-					// class in order to use it as a reference in the CSS code
-					$('#example1').addClass('custom-responsive');
-					$('#example1').accordionSlider('responsiveMode', 'custom');
-
-					// change the arrows' visibility
-					$('.custom-arrow').show();
-					$('.auto-arrow').hide();
-				}
-			});
-		
-
-
 /* accordion layers */
+$('#example1').accordionSlider({
+	width: 2000,
+	// orientation: "horizontal",
+	height: 525,
+	startPanel: 0,
+	shadow: 0,
+	responsiveMode: 'auto',
+	openedPanelSize: '60%',
+	maxOpenedPanelSize: '100%',
+	visiblePanels: 3,
+	closePanelsOnMouseOut: false,
+	autoplay: false
+});
+
+// change the responsive mode
+$('.controls a').click(function(event) {
+	event.preventDefault();
+
+	if ($(this).hasClass('auto')) {
+		// change the responsive mode to 'auto' and remove the 'custom-responsive' class
+		$('#example1').removeClass('custom-responsive');
+		$('#example1').accordionSlider('responsiveMode', 'auto');
+
+		// change the arrows' visibility
+		$('.auto-arrow').show();
+		$('.custom-arrow').hide();
+	} else if ($(this).hasClass('custom')) {
+		// change the responsive mode to 'custom' and add the 'custom-responsive' 
+		// class in order to use it as a reference in the CSS code
+		$('#example1').addClass('custom-responsive');
+		$('#example1').accordionSlider('responsiveMode', 'custom');
+
+		// change the arrows' visibility
+		$('.custom-arrow').show();
+		$('.auto-arrow').hide();
+	}
+});
+
 // $("#accordion1").awsAccordion({
 //   type:"horizontal",
 //   cssAttrsHor:{
